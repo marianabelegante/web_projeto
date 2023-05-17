10ed98555aadb7f4e871cc06979acd24dca75e37
marianabelegante <marianabelegante2018@gmail.com>
2023-05-16 22:16:55 -0400

    16-05

---
+++ scripts/aula2.js
@@ -0,0 +1,31 @@
+
+// var n1 = parseInt(prompt('Informe o primeiro valor: '))
+// var n2 = parseInt(prompt('Informe o segundo valor: '))
+
+// parseFloat() para converter para números reais...
+
+//alert(n1+n2);
+/*
+result.innerHTML  = n1+n2;
+
+var resp = confirm("Podemos continuar?");
+
+document.write("<p>" + resp + "</p>");
+
+//console.log(n1+n2);
+
+var n1 = parseInt(prompt('Informe o primeiro número: '))
+var n2 = parseInt(prompt('Informe o segundo número: '))
+var n3 = parseInt(prompt('Informe o terceiro número: '))
+var n4 = parseInt(prompt('Informe o quarto número: '))
+
+alert((n1+n2+n3+n4) / 4);
+
+var s = parseInt(prompt('Informe o seu saldo: '))
+
+alert((s * 2 / 100) + s);
+*/
+var t = parseInt(prompt('Informe a quantidade de segundos: '))
+var m = t / 60
+var s = t % 60
+alert(t + ' segundos equivale a ' + m + ' minutos e ' + s + ' segundos.' );
